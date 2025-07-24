0d3a025e588a2d57e356b35dd4b86a16f617a00c
Sara Sundin <sarasundin@rocketmail.com>
2025-07-24 10:46:26 +0200

    Add media folder

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
