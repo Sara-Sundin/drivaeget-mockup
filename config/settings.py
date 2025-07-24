3123e7be90048012fff9737c7424d22c4c21158a
Sara Sundin <sarasundin@rocketmail.com>
2025-07-24 15:41:51 +0200

    Fix staticfiles storage for Cloudinary

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,4 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
