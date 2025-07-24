a66c8941e333cd115a163d76a4b9025ca651a21a
Sara Sundin <sarasundin@rocketmail.com>
2025-07-24 16:31:07 +0200

    Add signup and login templates and urls

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,9 +10,22 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # 👈 bloggen visas direkt på /
     path('summernote/', include('django_summernote.urls')),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Authentication views
+    path('signup/', CreateView.as_view(
+        template_name='registration/signup.html',
+        form_class=UserCreationForm,
+        success_url='/'
+    ), name='signup'),
+    
+    path('login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/'
+    ), name='logout'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
